892c6349ad5f11f36b951c8fcfb01cb120f64871
iceman5508 <isaacparker94@gmail.com>
2017-04-18 20:50:26 -0400

    Create Message Interface
    Extended the server_interface.py class to create the message class.
    Also made minor edits to server_interface class.

--- src/server_interface.py
+++ src/server_interface.py
@@ -4,15 +4,15 @@ class ServerInterface:
     __metaclass__ = abc.ABCMeta
 
     @abc.abstractmethod
-    def get_message(self):
+    def get_message(self, id):
         pass
 
     @abc.abstractmethod
-    def post_message(self, content):
+    def post_message(self, to, subject, body):
         pass
 
     @abc.abstractmethod
-    def edit_message(self, content):
+    def edit_message(self, id, content):
         pass
 
     @abc.abstractmethod
