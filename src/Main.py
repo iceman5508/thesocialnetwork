3adaa88562ffc51ea3317d957ea0c835365d39cb
Isaac Parker <isaacparker94@gmail.com>
2017-04-24 16:55:28 -0400

    Upload Updated files
    Resolved Merge conflicts

--- src/Main.py
+++ src/Main.py
@@ -1,37 +1,34 @@
 from kivy.app import App
 from kivy.clock import Clock
-from view.Screens import Screens
-from ui.message_ui import MessageScreen
-from ui.new_message_ui import ComposeScreen
+from Screens import Screens
+from message_ui import MessageScreen
+from new_message_ui import ComposeScreen
 
 '''
 Social Messaging application
 Provide messaging and posting services
 to users
 '''
-class social():
-    
-   
-    
-    def __init__(self): 
-        
-        
-        #message screen
-        message =   MessageScreen("messages")
-        message.on_init()
-        
-        #this is temp and should be removed once first screen is made
-        message.make_active() 
-        
-        #new message screen       
-        new_message = ComposeScreen("new_messages")
-        new_message.on_init()  
-        
 
-        
+
+class social():
+
+    def __init__(self):
+
+        '''message screen'''
+        message = MessageScreen("messages")
+        message.on_init()
+
+        '''this is temp and should be removed once first screen is made'''
+        message.make_active()
+
+        '''new message screen'''
+        new_message = ComposeScreen("new_messages")
+        new_message.on_init()
+
 
 class socialApp(App):
- 
+
     def build(self):
         social()
         Clock.schedule_interval(Screens._manager.update, 1.0/60.0)
--- src/Screens.py
+++ src/Screens.py
@@ -2,38 +2,37 @@
 Created on Apr 3, 2017
 
 @author: isaac
-@summary: This class helps to create screens a lot easier 
+@summary: This class helps to create screens a lot easier
 as well as work in conjunction with the scene manager.
 '''
 from kivy.uix.screenmanager import Screen
 from abc import ABCMeta, abstractmethod
 from SceneManager import SceneManager
-from kivy.graphics import Color,Rectangle
+from kivy.graphics import Color, Rectangle
 from kivy.core.window import Window
 
 
 class Screens(Screen):
     '''
     Screens is an abstract class that blueprints
-    the creation of kivy screens. This class works 
+    the creation of kivy screens. This class works
     closely with the SceneManager to make transition of screens easier.
     Note that the only parent class method that should be called is the on_init
     method
     '''
     __metaclass__ = type('Screens', (type(Screen), ABCMeta), {})
-    
+
     _manager = SceneManager()
-   
+
     '''
-    This is the default constructor.
-    Here in the constructor 
+    This is the default constructor. Here in the constructor
     '''
     def __init__(self, name):
         '''
         Constructor
         '''
-        Screen.__init__(self)        
-        self.name = name       
+        Screen.__init__(self)
+        self.name = name
         Screens._manager.add_screen(self)
 
     '''
@@ -52,19 +51,19 @@ class Screens(Screen):
     '''
     def make_active(self):
         Screens._manager.active_screen(self.name)
-    
+
     '''
-    This method is used to init all properties 
+    This method is used to init all properties
     for the specific instance of the screens class
     this should be the last class method called
-    '''         
-    @abstractmethod   
+    '''
+    @abstractmethod
     def on_init(self):
-        pass  
-    
+        pass
+
     '''
     This method will update all objects painted on screen
-    '''         
-    @abstractmethod   
+    '''
+    @abstractmethod
     def update(self):
-        pass  
+        pass
